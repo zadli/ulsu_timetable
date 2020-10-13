b72bb22d83ae714091054f227124610178616a06
Zadli Musical <00vopoj00@gmail.com>
2020-10-13 21:31:41 +0400

    add some animations on start screen

--- app/src/main/java/ru/zadli/ulsu_collaborating/timetable/adapters/LaunchRVAdapter.java
+++ app/src/main/java/ru/zadli/ulsu_collaborating/timetable/adapters/LaunchRVAdapter.java
@@ -2,9 +2,13 @@ package ru.zadli.ulsu_collaborating.timetable.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +20,7 @@ import ru.zadli.ulsu_collaborating.timetable.R;
 public class LaunchRVAdapter extends RecyclerView.Adapter<LaunchRVAdapter.ViewHolder> {
     int[] myImageList = new int[]{R.drawable.first_week, R.drawable.second_week, R.drawable.events, R.drawable.settings};
     Context context;
+    int anim = 0;
 
     public LaunchRVAdapter(Context context) {
         this.context = context;
@@ -30,7 +35,24 @@ public class LaunchRVAdapter extends RecyclerView.Adapter<LaunchRVAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        coolAnimation1(holder.itemView);
+        coolAnimation2(holder.itemView);
         holder.picture.setImageResource(myImageList[position]);
+    }
+    private void coolAnimation1(View viewToAnimate) {
+        if (anim == 0) {
+            Animation animation = AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            anim = 1;
+        } else if(anim == 1) {
+            Animation animation = AnimationUtils.loadAnimation(context, R.anim.slide_in_right);
+            viewToAnimate.startAnimation(animation);
+            anim = 0;
+        }
+    }
+    private void coolAnimation2(View view) {
+        view.setTranslationY((float) Resources.getSystem().getDisplayMetrics().heightPixels);
+        view.animate().translationY(0.0f).setInterpolator(new DecelerateInterpolator(4.0f)).setDuration(700).start();
     }
 
     @Override
